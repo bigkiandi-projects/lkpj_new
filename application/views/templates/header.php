b05c2d952715afb2c3a3c0914000fb96c0a61c28
Bigkiandi <bigkianditeam@gmail.com>
2024-01-29 18:24:21 +0900

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,7 @@ if (!$user) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="Cache-Control" content="no-cache" />
   <meta name="base_url" content="<?php echo base_url() ?>">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <title><?php echo $judul ?></title>
