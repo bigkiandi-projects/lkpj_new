b974599df4170b9cd81a168ecc6a41583b8d76a1
Bigkiandi <bigkianditeam@gmail.com>
2024-01-30 14:58:25 +0900

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,11 @@
 <?php
 
 $this->db->join('role', 'id_role', 'left');
-$user = $this->db->get_where('user',['id_user' => $this->session->userdata('id_user')])->row_array();
+$user = $this->db->get_where('user',['id_user' => $this->session->userdata('id_user')])->row_array(); 
+if (!$user) {
+  session_destroy();
+  redirect('login','refresh')->deleteCookie('auth');
+}
 
 ?>
 <!DOCTYPE html>
